514882963fbad9e17a4843722f802e49528c441c
Zoe Sun <141170820+zoe-704@users.noreply.github.com>
2026-01-01 21:32:31 -0800

    Delete random_tests directory

--- random_tests/load_model.py
+++
@@ -1,8 +0,0 @@
-import torch
-from transformer_lens import HookedTransformer
-
-device = "mps" if torch.backends.mps.is_available() else "cpu"
-
-model = HookedTransformer.from_pretrained("pythia-160m").to(device)
-
-
